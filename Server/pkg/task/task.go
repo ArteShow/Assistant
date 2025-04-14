e63e26e16feae929639b961bc6dcbcb3fce0cfd0
ArteShow <sokartemax@gmail.com>
2025-04-14 15:09:49 +0200

    Created a new Package: task. Added these Methods: Newtask, SaveTask, GetAllUsersTasks, GetUserTaskByID and DeletUsersTaskByID. I also connected it with a database. Added a new note in notes

---
+++ Server/pkg/task/task.go
@@ -0,0 +1,118 @@
+package task
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Task struct {
+	ID          int64
+	Titel       string
+	Status      string
+	Description string
+}
+
+type User struct {
+	UserID int64
+	Tasks  []Task
+}
+
+func NewTask(titel, description, status string) *Task {
+	return &Task{
+		Titel:       titel,
+		Description: description,
+		Status:      status,
+	}
+}
+
+func SaveTask(task *Task, userID int64, db *sql.DB) error {
+	log.Printf("Inserting task '%s' for user ID %d into the database", task.Titel, userID)
+
+	query := `INSERT INTO tasks (titel, description, status, user_id) VALUES (?, ?, ?, ?)`
+
+	_, err := db.Exec(query, task.Titel, task.Description, task.Status, userID)
+	if err != nil {
+		log.Println("Error inserting task into database:", err)
+		return err
+	}
+
+	return nil
+}
+
+func GetAllUsersTasks(db *sql.DB, userID int64) ([]*Task, error) {
+	log.Printf("Getting all tasks for user ID %d from the database", userID)
+
+	query := `SELECT * FROM tasks WHERE user_id = ?`
+	rows, err := db.Query(query, userID)
+	if err != nil {
+		log.Println("Error getting tasks from database:", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []*Task
+	for rows.Next() {
+		task := &Task{}
+		err := rows.Scan(&task.ID, &task.Titel, &task.Description, &task.Status)
+		if err != nil {
+			log.Println("Error scanning task:", err)
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func GetUsersTaskByID(userID int64, taskID int64, db *sql.DB) (*Task, error) {
+    log.Printf("Getting task with ID %d for user ID %d", taskID, userID)
+
+    query := `SELECT id, titel, description, status FROM tasks WHERE user_id = ? AND id = ?`
+    rows, err := db.Query(query, userID, taskID)
+    if err != nil {
+        log.Println("Error getting task from database:", err)
+        return nil, err
+    }
+    defer rows.Close()
+
+    var task *Task
+    if rows.Next() {
+        task = &Task{}
+        err := rows.Scan(&task.ID, &task.Titel, &task.Description, &task.Status)
+        if err != nil {
+            log.Println("Error scanning task:", err)
+            return nil, err
+        }
+    } else {
+        log.Printf("No task found with ID %d for user ID %d", taskID, userID)
+        return nil, sql.ErrNoRows
+    }
+
+    return task, nil
+}
+
+func DeletUSersTaskByID(userID int64, taskID int64, db *sql.DB) (bool, error){
+	log.Printf("Deleting task with ID %d for user ID %d", taskID, userID)
+
+	query := `DELETE FROM tasks WHERE user_id = ? AND id = ?`
+	result, err := db.Exec(query, userID, taskID)
+	if err != nil {
+		log.Println("Error deleting task from database:", err)
+		return false, err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Println("Error getting rows affected:", err)
+		return false, err
+	}
+
+	if rowsAffected == 0 {
+		log.Printf("No task found with ID %d for user ID %d", taskID, userID)
+		return false, nil
+	}
+
+	return true, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ArteShow/HomeAssistant
 
 go 1.23.1
+
+require github.com/mattn/go-sqlite3 v1.14.27
